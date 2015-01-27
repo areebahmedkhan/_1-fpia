c18a399970576c1f932c507410b450cf7a22a3ca
Areeb Ahmed Khan <exeprint@live.com>
2015-01-27 14:20:44 +0500

    Feedback menus images and add data into table

--- Friends_of_PIA_dashboard/index.php
+++ Friends_of_PIA_dashboard/index.php
@@ -102,38 +102,38 @@ $query = new query();
                 <div class="widget-content">
                   <h6 class="bigstats">Friends of PIA Feedback Menu</h6>
                   <div id="big_stats" class="cf">
-                    <div class="stat"><i class="icon-anchor"></i> <span class="value"><?php echo $query->airport_service($connection->my_connection); ?></span> </div>
+                    <div class="stat"><i class="icon"><img src="img/airportservices-transparent.png" alt="Airport Services" width = 50></i> <span class="value"><?php echo $query->airport_service($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value"><?php echo $query->chekIn($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/checkin-transparent.png" width = 50 alt="Check In"></i> <span class="value"><?php echo $query->chekIn($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-twitter-sign"></i> <span class="value"><?php echo $query->Lounge_Services($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/lounge-transparent.png" width = 50 alt="Lounge Services"></i> <span class="value"><?php echo $query->Lounge_Services($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-bullhorn"></i> <span class="value"><?php echo $query->Boarding_Gate($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/boardinggate-transparent.png" width = 50 alt="Boarding Gate"></i> <span class="value"><?php echo $query->Boarding_Gate($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
  
                   </div>
 				  <div id="big_stats" class="cf">
-                    <div class="stat"> <i class="icon-anchor"></i> <span class="value"><?php echo $query->Punctuality($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/punctuality-transparent.png" width = 50 alt="Punctuality"></i> <span class="value"><?php echo $query->Punctuality($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value"><?php echo $query->IN_Flight_Services($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/inflightservices-transparent.png" width = 50 alt="In Flight Services"></i> <span class="value"><?php echo $query->IN_Flight_Services($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-twitter-sign"></i> <span class="value"><?php echo $query->Cabin_Crew($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/cabincrew-transparent.png" width = 50 alt="Cabin Crew"></i><span class="value"><?php echo $query->Cabin_Crew($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-bullhorn"></i> <span class="value"><?php echo $query->Meals($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/meals-transparent.png" width = 50 alt="Meals"></i> <span class="value"><?php echo $query->Meals($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
  
                   </div>
 				  <div id="big_stats" class="cf">
-                    <div class="stat"> <i class="icon-anchor"></i> <span class="value"><?php echo $query->Entertainment($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/entertainment-transparent.png" width = 50 alt="Entertainment"></i> <span class="value"><?php echo $query->Entertainment($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value"><?php echo $query->Cabin_Conditions($connection->my_connection); ?></span> </div>
+                    <div class="stat"> <i class="icon"><img src="img/cabincondition-transparent.png" width = 50 alt="Cabin Condition"></i> <span class="value"><?php echo $query->Cabin_Conditions($connection->my_connection); ?></span> </div>
                     <!-- .stat -->
                     
                     <div class="stat"> </div>
@@ -162,78 +162,63 @@ $query = new query();
             <!-- /widget-content 
           </div>-->
           <!-- /widget -->
-          <div class="widget">
-            <div class="widget-header"> <i class="icon-file"></i>
-              <h3> Content</h3>
+          <div class="widget widget-table action-table">
+            <div class="widget-header"> <i class="icon-th-list"></i>
+              <h3>All Feedback Records</h3>
             </div>
             <!-- /widget-header -->
             <div class="widget-content">
-              <ul class="messages_layout">
-                <li class="from_user left"> <a href="#" class="avatar"><img src="img/message_avatar1.png"/></a>
-                  <div class="message_wrap"> <span class="arrow"></span>
-                    <div class="info"> <a class="name">John Smith</a> <span class="time">1 hour ago</span>
-                      <div class="options_arrow">
-                        <div class="dropdown pull-right"> <a class="dropdown-toggle " id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#"> <i class=" icon-caret-down"></i> </a>
-                          <ul class="dropdown-menu " role="menu" aria-labelledby="dLabel">
-                            <li><a href="#"><i class=" icon-share-alt icon-large"></i> Reply</a></li>
-                            <li><a href="#"><i class=" icon-trash icon-large"></i> Delete</a></li>
-                            <li><a href="#"><i class=" icon-share icon-large"></i> Share</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="text"> As an interesting side note, as a head without a body, I envy the dead. There's one way and only one way to determine if an animal is intelligent. Dissect its brain! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. </div>
-                  </div>
-                </li>
-                <li class="by_myself right"> <a href="#" class="avatar"><img src="img/message_avatar2.png"/></a>
-                  <div class="message_wrap"> <span class="arrow"></span>
-                    <div class="info"> <a class="name">Bender (myself) </a> <span class="time">4 hours ago</span>
-                      <div class="options_arrow">
-                        <div class="dropdown pull-right"> <a class="dropdown-toggle " id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#"> <i class=" icon-caret-down"></i> </a>
-                          <ul class="dropdown-menu " role="menu" aria-labelledby="dLabel">
-                            <li><a href="#"><i class=" icon-share-alt icon-large"></i> Reply</a></li>
-                            <li><a href="#"><i class=" icon-trash icon-large"></i> Delete</a></li>
-                            <li><a href="#"><i class=" icon-share icon-large"></i> Share</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="text"> All I want is to be a monkey of moderate intelligence who wears a suit… that's why I'm transferring to business school! I had more, but you go ahead. Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. File not found. </div>
-                  </div>
-                </li>
-                <li class="from_user left"> <a href="#" class="avatar"><img src="img/message_avatar1.png"/></a>
-                  <div class="message_wrap"> <span class="arrow"></span>
-                    <div class="info"> <a class="name">Celeste Holm </a> <span class="time">1 Day ago</span>
-                      <div class="options_arrow">
-                        <div class="dropdown pull-right"> <a class="dropdown-toggle " id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#"> <i class=" icon-caret-down"></i> </a>
-                          <ul class="dropdown-menu " role="menu" aria-labelledby="dLabel">
-                            <li><a href="#"><i class=" icon-share-alt icon-large"></i> Reply</a></li>
-                            <li><a href="#"><i class=" icon-trash icon-large"></i> Delete</a></li>
-                            <li><a href="#"><i class=" icon-share icon-large"></i> Share</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="text"> And I'd do it again! And perhaps a third time! But that would be it. Are you crazy? I can't swallow that. And I'm his friend Jesus. No, I'm Santa Claus! And from now on you're all named Bender Jr. </div>
-                  </div>
-                </li>
-                <li class="from_user left"> <a href="#" class="avatar"><img src="img/message_avatar2.png"/></a>
-                  <div class="message_wrap"> <span class="arrow"></span>
-                    <div class="info"> <a class="name">Mark Jobs </a> <span class="time">2 Days ago</span>
-                      <div class="options_arrow">
-                        <div class="dropdown pull-right"> <a class="dropdown-toggle " id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#"> <i class=" icon-caret-down"></i> </a>
-                          <ul class="dropdown-menu " role="menu" aria-labelledby="dLabel">
-                            <li><a href="#"><i class=" icon-share-alt icon-large"></i> Reply</a></li>
-                            <li><a href="#"><i class=" icon-trash icon-large"></i> Delete</a></li>
-                            <li><a href="#"><i class=" icon-share icon-large"></i> Share</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="text"> That's the ONLY thing about being a slave. Now, now. Perfectly symmetrical violence never solved anything. Uh, is the puppy mechanical in any way? As an interesting side note, as a head without a body, I envy the dead. </div>
-                  </div>
-                </li>
-              </ul>
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th> Name </th>
+                    <th> Email </th>
+                    <th> Flight No </th>
+                    <th> Menu </th>
+                    <th> Comments </th>
+                    <th> Rating </th>
+                    <th> Image </th>
+                    <th> Date And Time </th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <?php $sql = "select u.name,u.email,u.flight_no, f.fd_id , f.date_time , c.title , sc.title as 'sub_title' , f.comments , f.ratting , f.image from catagories c, sub_catagories sc, feedback f, user_table u where u.user_id = f.user_id AND f.sub_cat_id = sc.sub_cat AND c.cat_id = sc.cat_id order by f.fd_id desc";
+                          $result = mysqli_query($connection->my_connection,$sql);
+                          if (mysqli_num_rows($result) > 0) {
+          while($row = mysqli_fetch_assoc($result)) {
+          echo "<tr>";
+          echo "<td>".$row['name']."</td>";
+          echo "<td><a href=''>".$row['email']."</a></td>";
+          echo "<td>".$row['flight_no']."</td>";
+  
+          echo "<td>".$row['sub_title']."</td>";
+          echo "<td>".$row['comments']."</td>";
+          //ratting smile
+          if($row['ratting']=='10'){
+            echo "<td>".'<img src="images/happy.png" width="50" height="50" />'."</td>";
+          }
+          else if($row['ratting']=='5'){
+            echo "<td>".'<img src="images/average.png" width="50" height="50" />'."</td>";
+          }
+          else if($row['ratting']=='1'){
+            echo "<td>".'<img src="images/sad.png" width="50" height="50" />'."</td>";
+          }else{
+            echo "<td>".'<img src="images/no-image.png" width="50" height="50" />'."</td>";
+          }
+                    
+          if($row['image']){
+            echo "<td>".'<img width="120" height="150" src="data:image/jpeg;base64,'.$row['image'].'" />'."</td>";
+          }else{
+            echo "<td>".'<img src="images/no-image.png" width="120" height="150" />'."</td>";
+          }
+          echo "<td>".$row['date_time']."</td>";
+          echo "</tr>";
+          }        
+        }
+                    ?>
+                
+                </tbody>
+              </table>
             </div>
             <!-- /widget-content --> 
           </div>
@@ -269,88 +254,9 @@ $query = new query();
             <!-- /widget-content --> 
           </div>
           <!-- /widget -->
-          <div class="widget widget-table action-table">
-            <div class="widget-header"> <i class="icon-th-list"></i>
-              <h3>A Table Example</h3>
-            </div>
-            <!-- /widget-header -->
-            <div class="widget-content">
-              <table class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th> Free Resource </th>
-                    <th> Download</th>
-                    <th class="td-actions"> </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td> Fresh Web Development Resources </td>
-                    <td> http://www.egrappler.com/ </td>
-                    <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                  </tr>
-                  <tr>
-                    <td> Fresh Web Development Resources </td>
-                    <td> http://www.egrappler.com/ </td>
-                    <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                  </tr>
-                  <tr>
-                    <td> Fresh Web Development Resources </td>
-                    <td> http://www.egrappler.com/ </td>
-                    <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                  </tr>
-                  <tr>
-                    <td> Fresh Web Development Resources </td>
-                    <td> http://www.egrappler.com/ </td>
-                    <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                  </tr>
-                  <tr>
-                    <td> Fresh Web Development Resources </td>
-                    <td> http://www.egrappler.com/ </td>
-                    <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                  </tr>
-                
-                </tbody>
-              </table>
-            </div>
-            <!-- /widget-content --> 
-          </div>
+          
           <!-- /widget --> 
-          <div class="widget widget-nopad">
-            <div class="widget-header"> <i class="icon-list-alt"></i>
-              <h3> Recent News</h3>
-            </div>
-            <!-- /widget-header -->
-            <div class="widget-content">
-              <ul class="news-items">
-                <li>
-                  
-                  <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Aug</span> </div>
-                  <div class="news-item-detail"> <a href="http://www.egrappler.com/thursday-roundup-40/" class="news-item-title" target="_blank">Thursday Roundup # 40</a>
-                    <p class="news-item-preview"> This is our web design and development news series where we share our favorite design/development related articles, resources, tutorials and awesome freebies. </p>
-                  </div>
-                  
-                </li>
-                <li>
-                  
-                  <div class="news-item-date"> <span class="news-item-day">15</span> <span class="news-item-month">Jun</span> </div>
-                  <div class="news-item-detail"> <a href="http://www.egrappler.com/retina-ready-responsive-app-landing-page-website-template-app-landing/" class="news-item-title" target="_blank">Retina Ready Responsive App Landing Page Website Template – App Landing</a>
-                    <p class="news-item-preview"> App Landing is a retina ready responsive app landing page website template perfect for software and application developers and small business owners looking to promote their iPhone, iPad, Android Apps and software products.</p>
-                  </div>
-                  
-                </li>
-                <li>
-                  
-                  <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Oct</span> </div>
-                  <div class="news-item-detail"> <a href="http://www.egrappler.com/open-source-jquery-php-ajax-contact-form-templates-with-captcha-formify/" class="news-item-title" target="_blank">Open Source jQuery PHP Ajax Contact Form Templates With Captcha: Formify</a>
-                    <p class="news-item-preview"> Formify is a contribution to lessen the pain of creating contact forms. The collection contains six different forms that are commonly used. These open source contact forms can be customized as well to suit the need for your website/application.</p>
-                  </div>
-                  
-                </li>
-              </ul>
-            </div>
-            <!-- /widget-content --> 
-          </div>
+         
           <!-- /widget -->
         </div>
         <!-- /span6 --> 
