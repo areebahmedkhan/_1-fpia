a325f9c7f3b6a9bf669492bd4e7aff70e803cc6c
muzafar49 <muzafar4996@gmail.com>
2015-01-27 16:50:36 +0500

    table images and csv formate button on index.php

--- Friends_of_PIA_dashboard/index.php
+++ Friends_of_PIA_dashboard/index.php
@@ -162,66 +162,7 @@ $query = new query();
             <!-- /widget-content 
           </div>-->
           <!-- /widget -->
-          <div class="widget widget-table action-table">
-            <div class="widget-header"> <i class="icon-th-list"></i>
-              <h3>All Feedback Records</h3>
-            </div>
-            <!-- /widget-header -->
-            <div class="widget-content">
-              <table class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th> Name </th>
-                    <th> Email </th>
-                    <th> Flight No </th>
-                    <th> Menu </th>
-                    <th> Comments </th>
-                    <th> Rating </th>
-                    <th> Image </th>
-                    <th> Date And Time </th>
-                  </tr>
-                </thead>
-                <tbody>
-                    <?php $sql = "select u.name,u.email,u.flight_no, f.fd_id , f.date_time , c.title , sc.title as 'sub_title' , f.comments , f.ratting , f.image from catagories c, sub_catagories sc, feedback f, user_table u where u.user_id = f.user_id AND f.sub_cat_id = sc.sub_cat AND c.cat_id = sc.cat_id order by f.fd_id desc";
-                          $result = mysqli_query($connection->my_connection,$sql);
-                          if (mysqli_num_rows($result) > 0) {
-          while($row = mysqli_fetch_assoc($result)) {
-          echo "<tr>";
-          echo "<td>".$row['name']."</td>";
-          echo "<td><a href=''>".$row['email']."</a></td>";
-          echo "<td>".$row['flight_no']."</td>";
-  
-          echo "<td>".$row['sub_title']."</td>";
-          echo "<td>".$row['comments']."</td>";
-          //ratting smile
-          if($row['ratting']=='10'){
-            echo "<td>".'<img src="images/happy.png" width="50" height="50" />'."</td>";
-          }
-          else if($row['ratting']=='5'){
-            echo "<td>".'<img src="images/average.png" width="50" height="50" />'."</td>";
-          }
-          else if($row['ratting']=='1'){
-            echo "<td>".'<img src="images/sad.png" width="50" height="50" />'."</td>";
-          }else{
-            echo "<td>".'<img src="images/no-image.png" width="50" height="50" />'."</td>";
-          }
-                    
-          if($row['image']){
-            echo "<td>".'<img width="120" height="150" src="data:image/jpeg;base64,'.$row['image'].'" />'."</td>";
-          }else{
-            echo "<td>".'<img src="images/no-image.png" width="120" height="150" />'."</td>";
-          }
-          echo "<td>".$row['date_time']."</td>";
-          echo "</tr>";
-          }        
-        }
-                    ?>
-                
-                </tbody>
-              </table>
-            </div>
-            <!-- /widget-content --> 
-          </div>
+          
           <!-- /widget --> 
         </div>
         <!-- /span6 -->
@@ -261,6 +202,70 @@ $query = new query();
         </div>
         <!-- /span6 --> 
       </div>
+      <div class="widget widget-table action-table" >
+            <div class="widget-header"> <i class="icon-th-list"></i>
+              <h3>All Feedback Records</h3><a style="float:right; margin-right:5px;" href="#" class="export"><input type="button" value="Export data in CSV"></a>
+            </div>
+            
+            <!-- /widget-header -->
+            <div class="widget-content">
+              <div id="dvData">
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th> Name </th>
+                    <th> Email </th>
+                    <th> Flight No </th>
+                    <th> Menu </th>
+                    <th> Comments </th>
+                    <th> Rating </th>
+                    <th> Image </th>
+                    <th> Date And Time </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                    <?php $sql = "select u.name,u.email,u.flight_no, f.fd_id , f.date_time , c.title , sc.title as 'sub_title' , f.comments , f.ratting , f.image from catagories c, sub_catagories sc, feedback f, user_table u where u.user_id = f.user_id AND f.sub_cat_id = sc.sub_cat AND c.cat_id = sc.cat_id order by f.fd_id desc";
+                          $result = mysqli_query($connection->my_connection,$sql);
+                          if (mysqli_num_rows($result) > 0) {
+                      while($row = mysqli_fetch_assoc($result)) {
+                      echo "<tr>";
+                      echo "<td><a href=''>".$row['name']."</a></td>";
+                      echo "<td><a href=''>".$row['email']."</a></td>";
+                      echo "<td><a href=''>".$row['flight_no']."</a></td>";
+              
+                      echo "<td><a href=''>".$row['sub_title']."</a></td>";
+                      echo "<td><a href=''>".$row['comments']."</a></td>";
+                      //ratting smile
+                      if($row['ratting']=='10'){
+                        echo "<td>".'<img src="images/happy.png" width="50" height="50" />'."</td>";
+                      }
+                      else if($row['ratting']=='5'){
+                        echo "<td>".'<img src="images/average.png" width="50" height="50" />'."</td>";
+                      }
+                      else if($row['ratting']=='1'){
+                        echo "<td>".'<img src="images/sad.png" width="50" height="50" />'."</td>";
+                      }else{
+                        echo "<td>".'<img src="images/no-image.png" width="50" height="50" />'."</td>";
+                      }
+                                
+                      if($row['image']){
+                        echo "<td>".'<a href="data:image/jpeg;base64,'.$row['image'].' " width="500" height="500" target="_blank"><img width="120" height="150" src="data:image/jpeg;base64,'.$row['image'].'" /></a>'."</td>";
+                      }else{
+                        echo "<td>".'<img src="images/no-image.png" width="120" height="150" />'."</td>";
+                      }
+                      echo "<td>".$row['date_time']."</td>";
+                      echo "</tr>";
+                      }        
+                    }
+                    ?>
+                
+                </tbody>
+              </table>
+            </div>
+            </div>
+            <!-- /widget-content --> 
+          </div>
       <!-- /row --> 
     </div>
     <!-- /container --> 
@@ -350,6 +355,63 @@ $query = new query();
 <script language="javascript" type="text/javascript" src="js/full-calendar/fullcalendar.min.js"></script>
  
 <script src="js/base.js"></script> 
+
+<script type="text/javascript">
+$(document).ready(function () {
+
+    function exportTableToCSV($table, filename) {
+
+        var $rows = $table.find('tr:has(td)'),
+
+            // Temporary delimiter characters unlikely to be typed by keyboard
+            // This is to avoid accidentally splitting the actual contents
+            tmpColDelim = String.fromCharCode(11), // vertical tab character
+            tmpRowDelim = String.fromCharCode(0), // null character
+
+            // actual delimiter characters for CSV format
+            colDelim = '","',
+            rowDelim = '"\r\n"',
+
+            // Grab text from table into CSV formatted string
+            csv = '"' + $rows.map(function (i, row) {
+                var $row = $(row),
+                    $cols = $row.find('td');
+
+                return $cols.map(function (j, col) {
+                    var $col = $(col),
+                        text = $col.text();
+
+                    return text.replace('"', '""'); // escape double quotes
+
+                }).get().join(tmpColDelim);
+
+            }).get().join(tmpRowDelim)
+                .split(tmpRowDelim).join(rowDelim)
+                .split(tmpColDelim).join(colDelim) + '"',
+
+            // Data URI
+            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+        $(this)
+            .attr({
+            'download': filename,
+                'href': csvData,
+                'target': '_blank'
+        });
+    }
+
+    // This must be a hyperlink
+    $(".export").on('click', function (event) {
+        // CSV
+        exportTableToCSV.apply(this, [$('#dvData>table'), 'PIA_Report.csv']);
+        
+        // IF CSV, don't do event.preventDefault() or return false
+        // We actually need this to be a typical hyperlink
+    });
+});
+
+</script>
+
 <script>     
 
         var lineChartData = {
